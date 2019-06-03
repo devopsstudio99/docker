4b2aca95484e98d4beef03e8a38b98cf928b7d77
devopsstudio99 <50159118+devopsstudio99@users.noreply.github.com>
2019-06-03 17:35:12 +0700

    Add files via upload

---
+++ react/frontend/build/precache-manifest.99e1ed75448ebb0072700dc20e71e54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2968718eb61ea1414a3f50184a34d70b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63c9572545d0969d7210",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "63c9572545d0969d7210",
+    "url": "/static/js/main.4f2b16b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
